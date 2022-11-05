cc630e2c63cc444d6be8d4b0d675e96a26c11bf3
Johnson <johnson@github.com>
2022-11-05 16:58:49 -0300

    Injetando o sessão do banco diretamente.

--- src/utility/userAuth.py
+++ src/utility/userAuth.py
@@ -4,11 +4,12 @@ from providers import check_access_token
 from fastapi import Depends, HTTPException, status
 from jose import JWTError
 from validation import UserValidation
+from configuration import generate_session
+
 
 oauth1_schema = OAuth2PasswordBearer(tokenUrl="token")
 
-
-def get_authenticated_user(session: Session, token: str = Depends(oauth1_schema)):
+def get_authenticated_user(session: Session = Depends(generate_session), token: str = Depends(oauth1_schema)):
     exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED, 
         detail='Token invalido'
