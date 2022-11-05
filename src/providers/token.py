b76b8274006442a1b2600ce22a34db24a1ef3963
Johnson <johnson@github.com>
2022-11-05 16:05:23 -0300

    Criado o gerador de token de acesso.

---
+++ src/providers/token.py
@@ -0,0 +1,27 @@
+from datetime  import datetime, timedelta
+from jose import jwt 
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITMO = os.getenv('ALGORITMO')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+
+
+def create_access_token(data: dict):
+    to_encode  = data.copy()
+    expiration = datetime.utcnow() + timedelta(minutes=EXPIRE_TOKEN)
+
+    to_encode.update({"exp": expiration})
+    
+    encoded_jwt  = jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITMO])   
+    return encoded_jwt
+
+
+def check_access_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITMO])
+    
+    return payload.get("sub")
